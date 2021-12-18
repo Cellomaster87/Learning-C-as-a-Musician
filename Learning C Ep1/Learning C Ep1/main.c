0ad87ad0e97b56437ea3d2ec1121736f06a036e6
Michele Galvagno <michicello@gmail.com>
2021-12-18 16:56:53 +0100

    Ep 1: first commit

---
+++ Learning C Ep1/Learning C Ep1/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Learning C Ep1
+//
+//  Created by Michele Galvagno on 18/12/21.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
