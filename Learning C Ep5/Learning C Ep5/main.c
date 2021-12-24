6e45cb30e81e11723378a114237c75fa99a869a4
Michele Galvagno <michicello@gmail.com>
2021-12-24 18:30:09 +0100

    Episode 5 completed.

---
+++ Learning C Ep5/Learning C Ep5/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Learning C Ep5
+//
+//  Created by Michele Galvagno on 24/12/21.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // There was no code in this lesson
+    // See you in episode 6!
+    
+    return 0;
+}
