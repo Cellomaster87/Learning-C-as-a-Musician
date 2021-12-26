86f4edf3833a410b2c93cf62680d534dba3a457b
Michele Galvagno <michicello@gmail.com>
2021-12-26 17:56:02 +0100

    Episode 7 source code

---
+++ Learning C Ep7/Learning C Ep7/main.c
@@ -0,0 +1,23 @@
+//
+//  main.c
+//  Learning C Ep7
+//
+//  Created by Michele Galvagno on 26/12/21.
+//
+
+#include <stdio.h>
+
+int sum(int x, int y);
+
+int main(int argc, const char * argv[]) {
+    printf("The sum of 3 and 2 is %d\n", sum(3, 2));
+    z = 4;
+    
+    return 0;
+}
+
+int sum(int x, int y) {
+    int z = x + y;
+    
+    return z;
+}
