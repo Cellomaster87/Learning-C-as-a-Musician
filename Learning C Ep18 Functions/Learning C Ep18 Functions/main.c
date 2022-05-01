80620bb1dd00c1fe5a69765e1dbc51dc4d187f3a
Michele Galvagno <michicello@gmail.com>
2022-05-01 20:24:29 +0200

    Episode 18 on Functions completed

--- Learning C Ep18 Functions/Learning C Ep18 Functions/main.c
+++ Learning C Ep18 Functions/Learning C Ep18 Functions/main.c
@@ -8,6 +8,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define kTuning 442
+#define OPENRANGE(value, low, high) (((value) >= (low)) && ((value) <= (high)))
+#define CLOSEDRANGE(value, low, high) (((value) >= (low)) && ((value) < (high)))
+
 void play_PartA(void);
 
 int main(int argc, const char * argv[]) {
@@ -35,6 +39,31 @@ int main(int argc, const char * argv[]) {
     
     play_PartA();
     
+    // MARK: - Declaration and definition
+    // This is a function declaration, inside the main function
+    void printTimeSignature(int above, int below);
+    
+    // Back into main, we call it, passing 'arguments' for its parameters
+    printTimeSignature(3, 4);
+    
+    // int myFunc(a, b);
+    int myF(void);
+    
+    // This is a function definition, inside the main function: not allowed
+    /*
+    int sum(int a, int b) {
+        return a + b;
+    }
+     */
+    
+    void printTempo(int mm);
+    printTempo(83);
+    printTempo(68);
+    
+    // MARK: - Macros
+    // Using macro values
+    printf("Tonight we are playing in %d\n", kTuning);
+    
     
     return 0;
 }
@@ -42,5 +71,42 @@ int main(int argc, const char * argv[]) {
 void play_PartA() {
     printf("Here are the notes for part A:\n");
     // here I would call the relevant part of the struct and return it
-    printf("Placeholder return text");
+    printf("Placeholder return text\n");
 };
+
+// This is the function definition, outside of the main function
+void printTimeSignature(int above, int below) {
+    printf("This piece is in %d/%d.\n", above, below);
+}
+
+void printTempo(int mm) {
+    char * tempo;
+    if (CLOSEDRANGE(mm, 144, 300)) tempo = "a Presto\n";
+    else if (OPENRANGE(mm, 120, 144)) tempo = "an Allegro\n";
+    else if (OPENRANGE(mm, 100, 120)) tempo = "a Moderato\n";
+    else if (OPENRANGE(mm, 80, 100)) tempo = "an Andante\n";
+    else if (OPENRANGE(mm, 60, 80)) tempo = "an Adagio\n";
+    else if (OPENRANGE(mm, 40, 60)) tempo = "a Largo\n";
+    else tempo = "too slow to bother!\n";
+    /*
+    if (mm >= 144) {
+        tempo = "a Presto\n";
+    } else if (mm >= 120 && mm < 144) {
+        tempo = "an Allegro\n";
+    } else if (mm >= 100 && mm < 120) {
+        tempo = "a Moderato\n";
+    } else if (mm >= 80 && mm < 100) {
+        tempo = "an Andante\n";
+    } else if (mm >= 60 && mm < 80) {
+        tempo = "an Adagio\n";
+    } else if (mm >= 40 && mm < 60) {
+        tempo = "a Largo\n";
+    } else {
+        tempo = "too slow to bother!\n";
+    }
+     */
+    printf("This movement is %s", tempo);
+//    printf("This movement is an %s", (mm >= 120) ? "Allegro" : "Moderato");
+}
+
+
